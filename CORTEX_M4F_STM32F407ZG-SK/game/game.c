90108575bab5384a37a0cd3d6b16d787fa6dfac5
Shaowu <breeze101792@gmail.com>
2016-03-22 18:42:54 +0800

    draw wall

--- CORTEX_M4F_STM32F407ZG-SK/game/game.c
+++ CORTEX_M4F_STM32F407ZG-SK/game/game.c
@@ -22,20 +22,34 @@
 typedef struct Car {
         uint16_t x;
         uint16_t y;
+        uint16_t x_old;
+        uint16_t y_old;
         uint8_t size;
-        uint16_t direction;
+        float direction;
+        float direction_old;
         uint8_t sensor_left;
         uint8_t sensor_right;
 } Car;
 
-uint64_t map[40];
+long long map[40]={
+        0x0,0x0,0xFFFFFF000000,0x000001000000,0x000001000000,
+        0x000001000000,0x000001000000,0x000001000000,0x000001000000,0x000001000000,
+        0x000001000000,0x000001000000,0x000001000000, 0x000001000000,0xFFE001000000,
+        0x2001000000,0x2001000000,0x2001000000,0x2001000000,0x2001000000,
+        0x2001000000,0x2001000000,0x2001000000,0x2001000000,0x2001000000,
+        0x2001000000, 0x2001FFFFFF, 0x2000000000,0x2000000000,0x2000000000,
+        0x2000000000,0x2000000000,0x2000000000,0x2000000000,0x2000000000,
+        0x2000000000,0x2000000000,0x2000000000,0x2000000000,0x3FFFFFFFFF
+};
+long long one = 0x1;
 
 Car mycar = {
-        .x = 120,
+        .x = 51,
         .y = 320,
         .size = 16,
-        .direction = 315
+        .direction = d2radian(271)
 };
+
 
 char* itoa(int i, char b[]){
         char const digit[] = "0123456789";
@@ -58,54 +72,45 @@ char* itoa(int i, char b[]){
 }
 
 int gameInitflag = 1;
-//
-// //Player2
-// int16_t player2X = LCD_PIXEL_WIDTH - 20;
-// int16_t player2Y = LCD_PIXEL_HEIGHT - 20;
-// uint16_t player2W = 60;
-// uint16_t player2H = 10;
-// uint8_t player2IsReversed = 0;
-//
-// //Ball
-// uint16_t ballSize = 5;
-// int16_t ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
-// int16_t ballY = ( LCD_PIXEL_HEIGHT - 5 ) - 30;// / 2;
-// int16_t ballVX = -3;
-// int16_t ballVY = -3;
-// uint8_t ballIsRun = 0;
-// uint8_t ballStepBack = 0;
-// uint16_t ballRadius = 3;//ballSize / 2;
-//Bricks
-uint16_t bricks[24];
-uint8_t bricksinit = 1;
 
 //Mode
 uint8_t demoMode = 0;
 
-// void
-// BallReset()
-// {
-//  ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
-//  ballY = ( LCD_PIXEL_HEIGHT - 5 ) -30;/// 2;
-//
-//  ballVX = -3;
-//  ballVY = -3;
-//
-//  ballIsRun = 1;
-// }
+
+void Draw_Wall(){
+        for (uint64_t i = 0; i < 40; i++) {//x
+                for (uint64_t j = 0; j < 48; j++) {//y
+                        if (!(map[i] & (one << j))) {
+                                LCD_SetTextColor( LCD_COLOR_BLACK );
+                                LCD_DrawFullRect( i * 6, topBarsize + j * 6, 4, 4 );
+                        }
+                        else{
+                                LCD_SetTextColor( LCD_COLOR_WHITE );
+                                LCD_DrawFullRect( i * 6, topBarsize + j * 6, 4, 4 );
+                        }
+                }
+        }
+}
 void Draw_Car(Car *x){
         char *buf[10];
         LCD_DrawCircle(x->x, x->y, x->size);
         // LCD_DisplayString(0,itoa(x->x,buf));
-        LCD_DrawUniLine(x->x, x->y, x->x - 24 * cos(d2radian(x->direction)), x->y + 24 * sin(d2radian(x->direction)));
+        LCD_DrawUniLine(x->x, x->y, x->x - 24 * cos(x->direction), x->y + 24 * sin(x->direction));
+}
+
+int sensor(x, y, dir, max = ){
+
 }
 void
 Game_Init()
 {
+
         LCD_SetColors(LCD_COLOR_RED, LCD_COLOR_BLACK);
-        LCD_SetTextColor(LCD_COLOR_WHITE);
-        // LCD_DisplayStringLine(0, "Hello");
-        Draw_Car(&mycar);
+        // LCD_SetTextColor(LCD_COLOR_WHITE);
+        char buf[10];
+        LCD_DisplayStringLine(0, itoa(cos(mycar.direction) * 1000000, buf));
+
+        Draw_Wall();
         // for (int16_t i = 0; i < 24;i++){
         //   bricks[i] = 0xffff;
         // }
@@ -152,18 +157,37 @@ GAME_Update()
                 Game_Init();
                 gameInitflag = 0;
         }
-        mycar.x -= 2 * cos(d2radian(mycar.direction));
-        mycar.y += 2 * sin(d2radian(mycar.direction));
+        mycar.x_old = mycar.x;
+        mycar.y_old = mycar.y;
+        mycar.direction_old = mycar.direction;
+        // mycar.x -= 2 * 1;
+        mycar.y -= 2 * 1;
+
+        // mycar.x -= 2 * cos(d2radian(mycar.direction));
+        // mycar.y += 2 * sin(d2radian(mycar.direction));
 
 }
 
 void
 GAME_Render()
 {
-        LCD_SetTextColor( LCD_COLOR_RED );
+
+        LCD_SetTextColor( LCD_COLOR_BLACK );
+
+        LCD_DrawCircle(mycar.x_old, mycar.y_old, mycar.size);
+        LCD_DrawUniLine(mycar.x_old, mycar.y_old, mycar.x_old - 24 * cos(mycar.direction_old), mycar.y_old + 24 * sin(mycar.direction_old));
+
+
+
+
+        LCD_SetTextColor( LCD_COLOR_WHITE );
         LCD_DrawLine(  0, LCD_PIXEL_HEIGHT - buttonPadding, LCD_PIXEL_WIDTH, LCD_DIR_HORIZONTAL );
 
-				Draw_Car(&mycar);
+        LCD_SetTextColor( LCD_COLOR_RED );
+        LCD_DrawCircle(mycar.x, mycar.y, mycar.size);
+        LCD_DrawUniLine(mycar.x, mycar.y, mycar.x - 24 * cos(mycar.direction), mycar.y + 24 * sin(mycar.direction));
+        // LCD_DrawUniLine(mycar.x, mycar.y, mycar.x + 24, mycar.y + 24);
+
 // LCD_SetBackColor(LCD_COLOR_BLACK);
 
 
